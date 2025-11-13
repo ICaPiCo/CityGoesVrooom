16e605e1d8c5bb79bc8fb86dc965a236f1e5d28d
sam1onearth <samuel@greenspinach.com>
2025-11-13 11:02:39 +0100

    Push
    Push OUSH

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-import pyxel, random
+import pyxel,random
 
 class City:
     def __init__(self):
         pyxel.init(600,950, title="City Generator and NPCs", display_scale=2)
         pyxel.run(self.update,self.draw)
-
-    def updtae(self):
+    def gen_world(self):
+        #build buildings, place buildings
+        #
+        pass
+    def update(self):
         pass
 
     def draw(self):
         pass
+    def create_building(self,pos):
+        '''takes a position and returns a list of coords for a building placement with 90° angles'''
+        x,y = pos
+        pos_list = []
+        
+        return pos_list
+        pass
+
+    def building_place(self,pos):
+            pyxel.line(pos[0],pos[1],pos[2],pos[3],7)
+            pyxel.line(pos[0], pos[1], pos[2], pos[3], 7)
+            pyxel.line(pos[0], pos[1], pos[2], pos[3], 7)
+            pyxel.line(pos[0], pos[1], pos[2], pos[3], 7)
+
+'''what we want:
+buildings (obstacles), roads (pats)--> coords? --> Blocky or continuous?'''
